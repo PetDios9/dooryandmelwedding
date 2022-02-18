aab2cfcd936745f0241b8b8009b79580a26a6a17
Peter Diosdado <diosdadopeter@gmail.com>
2022-02-17 20:24:03 -0800

    added schedule page with styling, added routing for schedule page

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,7 +1,89 @@
+import {
+  Button, Grid, Link, Typography,
+} from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EventIcon from '@mui/icons-material/Event';
+import TheRiverMill from '../images/TheRiverMill.jpg';
+import schedulePageStyles from '../styles/schedulePageStyles';
 
 export default function Schedule() {
   return (
-    <div>Schedule</div>
+    <Box sx={schedulePageStyles.root}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+      >
+        <Grid item>
+          <Typography textAlign="center" variant="h2">
+            schedule
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Box
+            component="img"
+            alt="The bride and groom"
+            sx={schedulePageStyles.imgContainer}
+            src={TheRiverMill}
+          />
+        </Grid>
+        <Grid item>
+          <Typography textAlign="center" variant="h6">
+            We can&apos;t wait to see you!
+            <hr />
+          </Typography>
+        </Grid>
+        <Grid item>
+          {/* Ceremony & Reception container */}
+          <Grid container spacing={6} sx={schedulePageStyles.ceremonyReceptionContainer}>
+            <Grid item xs={12}>
+              <LocationOnIcon />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography textAlign="center" variant="h2">
+                Ceremony &amp; Reception
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography textAlign="center" variant="h4">
+                The River Mill
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography textAlign="center" variant="h5">
+                1672 West Bowman Road
+                <br />
+                French Camp, California
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Link
+                href="https://goo.gl/maps/xTwZtZ6cQDwHHQVM7"
+                target="_blank"
+                rel="noreferrer noopener"
+                underline="none"
+              >
+                <Button variant="outlined" size="large">
+                  Map
+                </Button>
+              </Link>
+            </Grid>
+            <Grid item xs={12}>
+              <EventIcon />
+              <br />
+              <Typography textAlign="center" variant="h5">
+                Friday, October 7th, 2022
+                <br />
+                5:00 pm - 11:00 pm
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
---
+++ src/styles/schedulePageStyles.js
@@ -0,0 +1,19 @@
+const schedulePageStyles = {
+  root: {
+    color: '#4a4a4b',
+    padding: '2rem 0rem',
+    overflow: 'hidden',
+  },
+  imgContainer: {
+    height: 'auto',
+    width: '100%',
+    maxHeight: '50vh',
+    objectFit: 'cover',
+    // paddingTop: '5vh',
+  },
+  ceremonyReceptionContainer: {
+    padding: '3rem 5rem 3rem 5rem',
+  },
+};
+
+export default schedulePageStyles;
